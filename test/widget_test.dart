47fc099cf1771f659271f1f2bd95ddfcdfd82cad
jimmybong98 <167213072+jimmybong98@users.noreply.github.com>
2025-08-18 15:52:51 -0300

    Resolve API URL builder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,25 +6,17 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:relic_ttprod/main.dart';
+import 'package:relic_ttprod/app.dart';
+import 'package:relic_ttprod/features/preparacao/presentation/widgets/preparacao_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('App builds home page', (WidgetTester tester) async {
+    await tester.pumpWidget(const ProviderScope(child: App()));
+    expect(find.byType(PreparacaoPage), findsOneWidget);
+    // Check that the expected title is shown
+    expect(find.text('Preparação de OS'), findsOneWidget);
   });
 }
